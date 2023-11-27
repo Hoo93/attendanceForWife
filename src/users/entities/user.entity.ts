483cb1fb4b7bd83691cbb1de6f6dda26903bb1cc
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-11-27 23:23:43 +0900

    [ADD] User Entity 구현

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,26 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseTimeEntity } from '../BaseTimeEntity';
+
+@Entity()
+export class User extends BaseTimeEntity {
+  @PrimaryGeneratedColumn('uuid')
+  no: string;
+
+  @Column()
+  id: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  phoneNumber: string;
+
+  @Column()
+  age?: number;
+
+  @Column()
+  email?: string;
+}
