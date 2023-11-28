0faeda9f2f740dacc27e8809e1e86f1cc9808167
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-11-29 01:07:16 +0900

    [EDIT] 정규표현식 오류 수정

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -13,23 +13,23 @@ export class CreateAuthDto {
   @IsString()
   @MinLength(6)
   @MaxLength(12)
-  @Matches('/^[가-힣a-zA-Z0-9]+$/')
+  @Matches(/^[가-힣a-zA-Z0-9]+$/)
   id: string;
 
   @IsString()
   @MinLength(6)
   @MaxLength(12)
-  @Matches('/^[a-zA-Z0-9!@#$%]+$/')
+  @Matches(/^[a-zA-Z0-9!@#$%]+$/)
   password: string;
 
   @IsString()
   @MinLength(6)
   @MaxLength(12)
-  @Matches('/^[가-힣a-zA-Z0-9]+$/')
+  @Matches(/^[가-힣a-zA-Z0-9]+$/)
   name: string;
 
   @IsString()
-  @Matches('/^01[01]{1}-\\d{3,4}-\\d{4}$/')
+  @Matches(/^01[01]{1}-\d{3,4}-\d{4}$/)
   phoneNumber: string;
 
   @IsNumber()
@@ -39,6 +39,6 @@ export class CreateAuthDto {
   @IsString()
   @IsOptional()
   @IsEmail()
-  @Matches('/^([a-z0-9_\\.-]+)@([\\da-z\\.-]+)\\.([a-z\\.]{2,6})$/')
+  @Matches(/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/)
   email?: string;
 }
