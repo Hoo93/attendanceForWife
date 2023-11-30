23abb07b52325584ae82a1d9fe620c02ff083c87
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-12-01 00:30:08 +0900

    [ADD] passport localStrategy 로직 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,13 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
+import { LocalStrategy } from './local.strategy';
+import { UsersModule } from '../users/users.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [UsersModule, PassportModule, TypeOrmModule.forFeature([User])],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy, User],
 })
 export class AuthModule {}
