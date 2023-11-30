29e320e24417b5f1ffe530e0d6919c1f7822054a
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-12-01 00:27:53 +0900

    [ADD] validate 함수 구현

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,13 +1,18 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { AuthService } from './auth.service';
+import { UnauthorizedException } from '@nestjs/common';
 
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
     super();
   }
 
-  async validate(name: string, password: string): Promise<any> {
-    return;
+  async validate(id: string, password: string): Promise<any> {
+    const user = await this.authService.validateUser(id, password);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
   }
 }
