03f47b9fb5b79e8515a53bf93467e24496604e6b
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-11-30 13:08:31 +0900

    [EDIT] create-auth.dto ERROR MESSAGE 정리

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,4 +1,3 @@
-import { Column } from 'typeorm';
 import {
   IsEmail,
   IsNumber,
@@ -8,28 +7,49 @@ import {
   MaxLength,
   MinLength,
 } from 'class-validator';
+import {
+  INVALID_EMAIL_MESSAGE,
+  INVALID_ID_MAX_LENGTH_MESSAGE,
+  INVALID_ID_MESSAGE,
+  INVALID_ID_MIN_LENGTH_MESSAGE,
+  INVALID_NAME_MAX_LENGTH_MESSAGE,
+  INVALID_NAME_MESSAGE,
+  INVALID_NAME_MIN_LENGTH_MESSAGE,
+  INVALID_PASSWORD_MAX_LENGTH_MESSAGE,
+  INVALID_PASSWORD_MESSAGE,
+  INVALID_PASSWORD_MIN_LENGTH_MESSAGE,
+  INVALID_PHONENUMBER_MESSAGE,
+} from '../const/error-message';
 
 export class CreateAuthDto {
   @IsString()
-  @MinLength(6)
-  @MaxLength(12)
-  @Matches(/^[가-힣a-zA-Z0-9]+$/)
+  @MinLength(6, { message: INVALID_ID_MIN_LENGTH_MESSAGE })
+  @MaxLength(12, { message: INVALID_ID_MAX_LENGTH_MESSAGE })
+  @Matches(/^[a-zA-Z0-9]+$/, {
+    message: INVALID_ID_MESSAGE,
+  })
   id: string;
 
   @IsString()
-  @MinLength(6)
-  @MaxLength(12)
-  @Matches(/^[a-zA-Z0-9!@#$%]+$/)
+  @MinLength(6, { message: INVALID_PASSWORD_MIN_LENGTH_MESSAGE })
+  @MaxLength(12, { message: INVALID_PASSWORD_MAX_LENGTH_MESSAGE })
+  @Matches(/^(?=.*?[a-zA-Z])(?=.*?\d)(?=.*?[!@#$%^&*]).{6,13}$/, {
+    message: INVALID_PASSWORD_MESSAGE,
+  })
   password: string;
 
   @IsString()
-  @MinLength(6)
-  @MaxLength(12)
-  @Matches(/^[가-힣a-zA-Z0-9]+$/)
+  @MinLength(6, { message: INVALID_NAME_MIN_LENGTH_MESSAGE })
+  @MaxLength(12, { message: INVALID_NAME_MAX_LENGTH_MESSAGE })
+  @Matches(/^[가-힣a-zA-Z0-9]+$/, {
+    message: INVALID_NAME_MESSAGE,
+  })
   name: string;
 
   @IsString()
-  @Matches(/^01[01]{1}-\d{3,4}-\d{4}$/)
+  @Matches(/^01[01]{1}-\d{3,4}-\d{4}$/, {
+    message: INVALID_PHONENUMBER_MESSAGE,
+  })
   phoneNumber: string;
 
   @IsNumber()
@@ -39,6 +59,8 @@ export class CreateAuthDto {
   @IsString()
   @IsOptional()
   @IsEmail()
-  @Matches(/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/)
+  @Matches(/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, {
+    message: INVALID_EMAIL_MESSAGE,
+  })
   email?: string;
 }
