9fefdde9aa805861b78e7ee7481bf7411ba8ffbc
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-12-01 00:30:28 +0900

    [ADD] signup 비지니스 로직, 컨트롤러 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
 import { Repository } from 'typeorm';
@@ -10,23 +9,18 @@ export class AuthService {
   constructor(
     @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  async signup(createAuthDto: CreateAuthDto) {
+    const user = createAuthDto.toEntity();
+    user.createId = user.id;
+    user.updateId = user.id;
+    return await this.userRepository.save(user);
   }
 
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  async validateUser(id: string, password: string) {
+    const user = await this.userRepository.findOne({ where: { id } });
+    if (user && user.password === password) {
+      const { password, ...result } = user;
+      return result;
+    }
   }
 }
