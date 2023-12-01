abb1694f8911f52275e3fcd36f5cb34011ab29bc
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-12-02 01:11:32 +0900

    [ADD] swagger 설명 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,23 +1,26 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
-import { CreateUserDto } from '../users/dto/create-user.dto';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { User } from '../users/entities/user.entity';
 
 @Controller('auth')
+@ApiTags('인증')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/signup')
-  async signup(@Body() createAuthDto: CreateAuthDto) {
+  @ApiOperation({ summary: '회원 가입' })
+  @ApiResponse({
+    status: 201,
+    description: '회원 가입',
+    type: User,
+  })
+  @ApiBody({
+    type: CreateAuthDto,
+    description: '회원 가입 DTO',
+  })
+  signup(@Body() createAuthDto: CreateAuthDto) {
     return this.authService.signup(createAuthDto);
   }
 }
