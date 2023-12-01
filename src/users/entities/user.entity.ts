bc4c4e526f2c09418060e5b196e9a6285d163804
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-12-02 00:42:01 +0900

    [ADD] swagger 데코레이터 추가

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -2,29 +2,37 @@ import { Column, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm';
 import { BaseTimeEntity } from '../BaseTimeEntity';
 import * as bcrypt from 'bcrypt';
 import { SALT } from '../../auth/const/auth.const';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 @Entity()
 @Unique(['id'])
 export class User extends BaseTimeEntity {
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryGeneratedColumn('uuid', { comment: '회원번호' })
+  @ApiProperty({ description: '회원번호' })
   no: string;
 
-  @Column()
+  @Column({ comment: '회원 아이디', type: 'string' })
+  @ApiProperty({ description: '회원 아이디', type: 'string' })
   id: string;
 
-  @Column()
+  @Column({ comment: '회원 비밀번호', type: 'string' })
+  @ApiProperty({ comment: '회원 비밀번호', type: 'string' })
   password: string;
 
-  @Column()
+  @Column({ comment: '회원 이름', type: 'string' })
+  @ApiProperty({ description: '회원 이름', type: 'string' })
   name: string;
 
-  @Column()
+  @Column({ comment: '회원 전화번호', type: 'string' })
+  @ApiProperty({ description: '회원 전화번호', type: 'string' })
   mobileNumber: string;
 
-  @Column({ nullable: true })
-  age?: number;
+  @Column({ nullable: true, comment: '회원 생년월일', type: 'string' })
+  @ApiPropertyOptional({ description: '회원 생년월일', type: 'string' })
+  age?: string;
 
-  @Column({ nullable: true })
+  @Column({ nullable: true, comment: '회원 이메일', type: 'string' })
+  @ApiPropertyOptional({ description: '회원 이메일', type: 'string' })
   email?: string;
 
   async hashPassword() {
