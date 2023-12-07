c2367165d8536c8b5f64cc3508baa15f5b149b9f
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-12-07 19:41:13 +0900

    [ADD][TEST] mockJwtService sign method 구현

--- src/auth/mockJwtService.ts
+++ src/auth/mockJwtService.ts
@@ -1,8 +1,5 @@
-import { User } from '../users/entities/user.entity';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { NotFoundException } from '@nestjs/common';
-import { plainToClass } from 'class-transformer';
-
 export class MockJwtService {
-  public sign(payload:any,options){}
+  public async sign(payload: any, options) {
+    return 'access_token';
+  }
 }
