f27a701f353250f1f61750ab7a536e3b20140a01
sanghoo93 <sanghoo.park@codecrain.com>
2023-12-08 13:04:17 +0900

    [ADD][TEST] findOneById 테스트 코드 작성

---
+++ test/unit/user.service.spec.ts
@@ -0,0 +1,47 @@
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from '../../src/users/entities/user.entity';
+import { MockUserRepository } from './mockUserRepository';
+import { Test, TestingModule } from '@nestjs/testing';
+import { MockJwtService } from './mockJwtService';
+import { UsersService } from "../../dist/users/users.service";
+
+// type MockRepository<T = any> = Partial<Record<keyof T, jest.Mock>>;
+// const mockRepository = () => ({
+//   findOne: jest.fn(),
+//   save: jest.fn(),
+//   create: jest.fn(),
+// });
+describe('UserService Test', function () {
+  let service: UsersService;
+  let userRepository: MockUserRepository;
+  let jwtService: MockJwtService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: MockUserRepository,
+        },
+      ],
+    }).compile();
+
+    service = module.get<UsersService>(UsersService);
+    userRepository = module.get<MockUserRepository>(getRepositoryToken(User));
+  });
+
+  it('usersService should be defined', function () {
+    expect(service).toBeDefined();
+    expect(userRepository).toBeDefined();
+  });
+
+  it('findOneById should returns exact user', async () => {
+    const id = 'TEST_1'
+
+    const user = await service.findOneById(id);
+
+    expect(user.id).toBe(id)
+
+  })
+});
