38f3484dd47d01e5e8e7c7c941933fa35633a41c
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-12-11 23:28:20 +0900

    [EDIT] DEV 환경 구성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ormConfig } from './orm.config';
+import { ormConfig, ormConfigDevelopment } from './orm.config';
 import { User } from './users/entities/user.entity';
 
 @Module({
   imports: [
     UsersModule,
     AuthModule,
-    TypeOrmModule.forRoot({ ...ormConfig, entities: [User] }),
+    TypeOrmModule.forRoot({ ...ormConfigDevelopment, entities: [User] }),
   ],
 })
 export class AppModule {}
