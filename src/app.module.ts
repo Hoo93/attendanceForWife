bedb2bc8104538cdd77d6afea21c06f6a0a66351
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-12-14 08:21:05 +0100

    [ADD] roles resource 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ormConfig, ormConfigDevelopment } from './orm.config';
 import { User } from './users/entities/user.entity';
+import { RolesModule } from './roles/roles.module';
 
 @Module({
   imports: [
     UsersModule,
     AuthModule,
     TypeOrmModule.forRoot({ ...ormConfigDevelopment, entities: [User] }),
+    RolesModule,
   ],
 })
 export class AppModule {}
