d11481a230cacd22de703ead90df59c26a5f52a6
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-12-15 05:04:35 +0100

    [ADD] role entity 구현

--- src/roles/entities/role.entity.ts
+++ src/roles/entities/role.entity.ts
@@ -1,1 +1,21 @@
-export class Role {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { RoleType } from '../role-type.enum';
+import { BaseTimeEntity } from '../../BaseTimeEntity';
+import { Unique } from 'typeorm/browser';
+
+@Entity()
+@Unique(['name'])
+export class Role extends BaseTimeEntity {
+  @PrimaryGeneratedColumn('increment', { comment: '권한번호' })
+  @ApiProperty({ description: '권한번호' })
+  id: string;
+
+  @Column({ comment: '권한 이름', type: 'varchar' })
+  @ApiProperty({ description: '권한 이름', type: 'string' })
+  name: string;
+
+  @Column({ comment: '권한 타입', type: 'enum', enum: RoleType })
+  @ApiProperty({ description: '권한 타입', type: RoleType })
+  type: RoleType;
+}
