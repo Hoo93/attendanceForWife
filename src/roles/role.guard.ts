448972629f7225fd4e47316c1e7fdcda60a03c88
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-12-17 07:46:02 +0100

    [ADD] role guard 임시 구현

---
+++ src/roles/role.guard.ts
@@ -0,0 +1,27 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { RoleType } from './role-type.enum';
+import { ROLES_KEY } from './role.decorator';
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!requiredRoles) {
+      return true; // No specific roles required, allow access
+    }
+
+    const { user } = context.switchToHttp().getRequest();
+
+    return requiredRoles.some((role) => user.roles?.includes(role));
+  }
+}
