ab49289f574b24495bf5fbca718a9297ff10d758
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-12-18 06:26:11 +0100

    [ADD] Attendance entity 구현

--- src/attendances/entities/attendance.entity.ts
+++ src/attendances/entities/attendance.entity.ts
@@ -1,3 +1,22 @@
 import { BaseTimeEntity } from '../../BaseTimeEntity';
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { AttendanceType } from '../attendance-type.enum';
 
-export class Attendance extends BaseTimeEntity {}
+export class Attendance extends BaseTimeEntity {
+  @PrimaryGeneratedColumn('uuid', { comment: '출석부 번호' })
+  @ApiProperty({ description: '출석부 번호' })
+  no: string;
+
+  @Column({ comment: '출석부 제목', type: 'varchar' })
+  @ApiProperty({ description: '출석부 제목', type: 'string' })
+  title: string;
+
+  @Column({ comment: '출석부 설명', type: 'varchar', nullable: true })
+  @ApiProperty({ description: '출석부 설명', type: 'string' })
+  description: string;
+
+  @Column({ comment: '출석부 타입', type: 'enum', enum: AttendanceType })
+  @ApiProperty({ description: '출석부 타입', type: AttendanceType })
+  type: AttendanceType;
+}
