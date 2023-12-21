4b413fa845e15558c4fa6e6db33daec35f07a03b
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-12-21 08:25:23 +0000

    [EDIT] App 모듈 설정 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,23 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ormConfig } from './orm.config';
+import { ormConfig, ormConfigDevelopment } from './orm.config';
 import { User } from './users/entities/user.entity';
-import { Attendance } from './attendances/entities/attendance.entity' ;
+import { Attendance } from './attendances/entities/attendance.entity';
 import { RolesModule } from './roles/roles.module';
 import { Role } from './roles/entities/role.entity';
+import { AttendancesModule } from './attendances/attendances.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      ...ormConfig,
+      entities: [User, Role, Attendance],
+    }),
     UsersModule,
     AuthModule,
-    TypeOrmModule.forRoot({ ...ormConfig, entities: [User, Role,Attendance] }),
     RolesModule,
+    AttendancesModule,
   ],
 })
 export class AppModule {}
--- src/orm.config.ts
+++ src/orm.config.ts
@@ -13,3 +13,17 @@ export const ormConfig: TypeOrmModuleOptions = {
   charset: 'utf8mb4',
   autoLoadEntities: true,
 };
+
+export const ormConfigDevelopment: TypeOrmModuleOptions = {
+  type: 'mysql',
+  port: 12300,
+  host: 'localhost',
+  username: 'test_user',
+  password: 'test_password',
+  database: 'attendance',
+  synchronize: true,
+  logging: true,
+  poolSize: 10,
+  charset: 'utf8mb4',
+  autoLoadEntities: true,
+};
