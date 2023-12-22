24d2085ee797efaa0b5c01a55382aaacf87caff7
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-12-22 23:08:30 +0100

    [ADD] createAttendanceDto 구현

--- src/attendances/dto/create-attendance.dto.ts
+++ src/attendances/dto/create-attendance.dto.ts
@@ -1,1 +1,32 @@
-export class CreateAttendanceDto {}
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import {} from '../../auth/const/error-message';
+import { ApiProperty } from '@nestjs/swagger';
+import { Optional } from '@nestjs/common';
+import { AttendanceType } from '../attendance-type.enum';
+
+export class CreateAttendanceDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: '출석부 제목',
+    type: 'string',
+    example: 'attendance title',
+  })
+  title: string;
+
+  @IsString()
+  @Optional()
+  @ApiProperty({
+    description: '출석부 설명',
+    type: 'string',
+    example: 'attendance description',
+  })
+  description: string;
+
+  @IsEnum(AttendanceType)
+  @ApiProperty({
+    description: '출석부 타입',
+    type: AttendanceType,
+  })
+  type: AttendanceType;
+}
