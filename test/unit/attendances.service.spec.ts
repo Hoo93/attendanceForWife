136158f9d09ac17387610093d1d0ded9a398041d
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-12-31 02:36:01 +0900

    [ADD] attendance.service 통합 테스트 코드 설정 구현

---
+++ test/unit/attendances.service.spec.ts
@@ -0,0 +1,27 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AttendancesService } from '../../src/attendances/attendances.service';
+import { Attendance } from '../../src/attendances/entities/attendance.entity';
+import { UserAttendance } from '../../src/attendances/entities/user-attendance.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppModule } from '../../src/app.module';
+
+describe('AttendancesService', () => {
+  let service: AttendancesService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        // TypeOrmModule.forRoot <- DB 커넥션을 위해 AppModule import
+        AppModule,
+        TypeOrmModule.forFeature([Attendance, UserAttendance]),
+      ],
+      providers: [AttendancesService],
+    }).compile();
+
+    service = module.get<AttendancesService>(AttendancesService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
