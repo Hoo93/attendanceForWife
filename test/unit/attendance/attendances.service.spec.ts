bca68743b11bbd7215915580405c8cf0f27bf7d3
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2024-01-04 22:35:06 +0900

    [ADD] findAllByUserId 테스트코드 구현

--- test/unit/attendance/attendances.service.spec.ts
+++ test/unit/attendance/attendances.service.spec.ts
@@ -33,7 +33,17 @@ describe('AttendancesService', () => {
 
     await userRepository.query(
       `INSERT INTO user SET 
-        id = 'user id' , 
+        id = 'user id 1' , 
+        username = 'test id',
+        password = 'testPWD',
+        mobileNumber = '010-8098-1398',
+        name = 'test name',
+        createId ='user id'`,
+    );
+
+    await userRepository.query(
+      `INSERT INTO user SET 
+        id = 'user id 2' , 
         username = 'test id',
         password = 'testPWD',
         mobileNumber = '010-8098-1398',
@@ -53,46 +63,93 @@ describe('AttendancesService', () => {
     expect(service).toBeDefined();
   });
 
-  it('출석부 테이블에 출석부를 생성한다.', async () => {
-    // given
-    const createAttendanceDto = new CreateAttendanceDto();
-    createAttendanceDto.title = 'test title';
-    createAttendanceDto.description = 'test description';
-    createAttendanceDto.type = AttendanceType.WEEKDAY;
+  describe('createAttendance Test', () => {
+    it('출석부 테이블에 출석부를 생성한다.', async () => {
+      // given
+      const createAttendanceDto = new CreateAttendanceDto();
+      createAttendanceDto.title = 'test title';
+      createAttendanceDto.description = 'test description';
+      createAttendanceDto.type = AttendanceType.WEEKDAY;
 
-    const user = new User();
-    user.id = 'user id';
+      const user = new User();
+      user.id = 'user id 1';
 
-    // when
-    const createdAttendanceId = await service.create(createAttendanceDto, user);
+      // when
+      const createdAttendanceId = await service.create(
+        createAttendanceDto,
+        user,
+      );
 
-    const newAttendance = await service.findOneById(createdAttendanceId.id);
+      const newAttendance = await service.findOneById(createdAttendanceId.id);
 
-    // then
-    expect(newAttendance.title).toBe('test title');
-    expect(newAttendance.description).toBe('test description');
-    expect(newAttendance.type).toBe(AttendanceType.WEEKDAY);
+      // then
+      expect(newAttendance.title).toBe('test title');
+      expect(newAttendance.description).toBe('test description');
+      expect(newAttendance.type).toBe(AttendanceType.WEEKDAY);
+    });
+
+    it('UserAttendance 테이블에 Admin 권한으로 데이터가 생성된다.', async () => {
+      // given
+      const createAttendanceDto = new CreateAttendanceDto();
+      createAttendanceDto.title = 'test title';
+      createAttendanceDto.description = 'test description';
+      createAttendanceDto.type = AttendanceType.WEEKDAY;
+
+      const user = new User();
+      user.id = 'user id 1';
+
+      // when
+      const createdAttendanceId = await service.create(
+        createAttendanceDto,
+        user,
+      );
+
+      const userAttendance = await userAttendanceRepository.query(
+        `SELECT * FROM user_attendance WHERE userId = 'user id 1'`,
+      );
+
+      // then
+      expect(userAttendance[0].role).toBe(RoleType.ADMIN);
+      expect(userAttendance[0].attendanceId).toBe(createdAttendanceId.id);
+    });
   });
 
-  it('출석부를 생성하면 조인테이블에 데이터가 생성된다.', async () => {
-    // given
-    const createAttendanceDto = new CreateAttendanceDto();
-    createAttendanceDto.title = 'test title';
-    createAttendanceDto.description = 'test description';
-    createAttendanceDto.type = AttendanceType.WEEKDAY;
+  describe('findAllByUserId Test', () => {
+    it('회원의 id로 생성된 모든 출석부를 조회한다.', async () => {
+      // given
+      const user_1 = new User();
+      user_1.id = 'user id 1';
 
-    const user = new User();
-    user.id = 'user id';
+      const user_2 = new User();
+      user_2.id = 'user id 2';
 
-    // when
-    const createdAttendanceId = await service.create(createAttendanceDto, user);
+      const createAttendanceDto_1 = new CreateAttendanceDto();
+      createAttendanceDto_1.title = 'test title 1';
+      createAttendanceDto_1.description = 'test description';
+      createAttendanceDto_1.type = AttendanceType.WEEKDAY;
 
-    const userAttendance = await userAttendanceRepository.query(
-      `SELECT * FROM user_attendance WHERE userId = 'user id'`,
-    );
+      const createAttendanceDto_2 = new CreateAttendanceDto();
+      createAttendanceDto_2.title = 'test title 2';
+      createAttendanceDto_2.description = 'test description';
+      createAttendanceDto_2.type = AttendanceType.WEEKDAY;
 
-    // then
-    expect(userAttendance[0].role).toBe(RoleType.ADMIN);
-    expect(userAttendance[0].attendanceId).toBe(createdAttendanceId.id);
+      const createAttendanceDto_3 = new CreateAttendanceDto();
+      createAttendanceDto_3.title = 'test title 3';
+      createAttendanceDto_3.description = 'test description';
+      createAttendanceDto_3.type = AttendanceType.WEEKDAY;
+
+      await service.create(createAttendanceDto_1, user_1);
+      await service.create(createAttendanceDto_2, user_1);
+      const createdByUserTwo = await service.create(
+        createAttendanceDto_3,
+        user_2,
+      );
+
+      // when
+      const sut = await service.findAllByUserId(user_1.id);
+      // then
+      expect(sut.length).toBe(2);
+      expect(sut).not.toContain(createdByUserTwo);
+    });
   });
 });
