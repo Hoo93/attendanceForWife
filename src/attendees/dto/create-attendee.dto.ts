0d0090347dd0c1074e15dc8f28586e3b82a8e35b
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2024-01-07 23:54:24 +0900

    [ADD] create-attendee.dto 구현

--- src/attendees/dto/create-attendee.dto.ts
+++ src/attendees/dto/create-attendee.dto.ts
@@ -1,1 +1,31 @@
-export class CreateAttendeeDto {}
+import { IsInt, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateAttendeeDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: '출석 대상 이름',
+    type: 'string',
+    example: 'attendee name',
+  })
+  name: string;
+
+  @IsInt()
+  @IsOptional()
+  @ApiProperty({
+    description: '출석 대상 나이',
+    type: 'int',
+    example: 15,
+  })
+  age: number;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({
+    description: '출석 대상 설명',
+    type: 'string',
+    example: 'attendee description',
+  })
+  description: string;
+}
