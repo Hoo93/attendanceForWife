73dd124854337716250ba94215ac8228674741d4
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2024-01-12 23:57:49 +0900

    [ADD] Schedule Entity 속성 구현

--- server/src/schedules/entities/schedule.entity.ts
+++ server/src/schedules/entities/schedule.entity.ts
@@ -1,5 +1,22 @@
-import {Entity} from "typeorm";
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
 import {BaseTimeEntity} from "../../BaseTimeEntity";
+import {ApiProperty} from "@nestjs/swagger";
 
 @Entity()
-export class Schedule extends BaseTimeEntity {}
+export class Schedule extends BaseTimeEntity {
+    @PrimaryGeneratedColumn('increment')
+    @ApiProperty({ description: '출석대상의 출석 스케쥴' })
+    id: number;
+
+    @Column({ comment: '출석 대상 ID', type: 'varchar' })
+    @ApiProperty({ description: '출석 대상 ID', type: 'string' })
+    attendeeId: string;
+
+    @Column({comment:'출석 요일',type:'enum'})
+    @ApiProperty({description:'출석 요일',type:'enum'})
+    day:string;
+
+    @Column({ comment: '출석 시간', type: 'varchar' })
+    @ApiProperty({ description: '출석 시간', type: 'string' })
+    time: string;
+}
