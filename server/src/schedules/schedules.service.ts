df0a2719782477b58b007dd631fee5726bbd38c2
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2024-01-13 00:04:01 +0900

    [ADD] scheduleRepository 주입

--- server/src/schedules/schedules.service.ts
+++ server/src/schedules/schedules.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateScheduleDto } from './dto/create-schedule.dto';
 import { UpdateScheduleDto } from './dto/update-schedule.dto';
+import {InjectRepository} from "@nestjs/typeorm";
+import {Schedule} from "./entities/schedule.entity";
+import {Repository} from "typeorm";
 
 @Injectable()
 export class SchedulesService {
+  constructor(
+      @InjectRepository(Schedule)
+      scheduleRepository:Repository<Schedule>
+  ) {}
+
   create(createScheduleDto: CreateScheduleDto) {
     return 'This action adds a new schedule';
   }
