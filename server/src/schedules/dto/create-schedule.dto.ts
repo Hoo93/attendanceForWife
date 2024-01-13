3f9215deddad7cf56def2e0328f612553b3394b5
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2024-01-13 16:13:58 +0900

    [ADD] schedule Entity , DTO 구현

--- server/src/schedules/dto/create-schedule.dto.ts
+++ server/src/schedules/dto/create-schedule.dto.ts
@@ -1,1 +1,18 @@
-export class CreateScheduleDto {}
+import { Column, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { DayType } from '../const/day-type.enum';
+import { Attendee } from '../../attendees/entities/attendee.entity';
+import { IsEnum, IsNotEmpty, IsString, Matches } from 'class-validator';
+
+export class CreateScheduleDto {
+  @IsString()
+  @IsNotEmpty()
+  attendeeId: string;
+
+  @IsEnum(DayType)
+  day: DayType;
+
+  @IsString()
+  @Matches(/^\d{4}$/)
+  time: string;
+}
--- server/src/schedules/entities/schedule.entity.ts
+++ server/src/schedules/entities/schedule.entity.ts
@@ -1,28 +1,35 @@
-import {Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn} from "typeorm";
-import {BaseTimeEntity} from "../../BaseTimeEntity";
-import {ApiProperty} from "@nestjs/swagger";
-import {Attendee} from "../../attendees/entities/attendee.entity";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { BaseTimeEntity } from '../../BaseTimeEntity';
+import { ApiProperty } from '@nestjs/swagger';
+import { Attendee } from '../../attendees/entities/attendee.entity';
+import { DayType } from '../const/day-type.enum';
 
 @Entity()
 export class Schedule extends BaseTimeEntity {
-    @PrimaryGeneratedColumn('increment')
-    @ApiProperty({ description: '출석대상의 출석 스케쥴' })
-    id: number;
+  @PrimaryGeneratedColumn('increment')
+  @ApiProperty({ description: '출석대상의 출석 스케쥴' })
+  id: number;
 
-    @Column({ comment: '출석 대상 ID', type: 'varchar' })
-    @ApiProperty({ description: '출석 대상 ID', type: 'string' })
-    attendeeId: string;
+  @Column({ comment: '출석 대상 ID', type: 'varchar' })
+  @ApiProperty({ description: '출석 대상 ID', type: 'string' })
+  attendeeId: string;
 
-    @Column({comment:'출석 요일',type:'enum'})
-    @ApiProperty({description:'출석 요일',type:'enum'})
-    day:string;
+  @Column({ comment: '출석 요일', type: 'enum', enum: DayType })
+  @ApiProperty({ description: '출석 요일', type: 'enum', enum: DayType })
+  day: DayType;
 
-    @Column({ comment: '출석 시간', type: 'varchar' })
-    @ApiProperty({ description: '출석 시간', type: 'string' })
-    time: string;
+  @Column({ comment: '출석 시간', type: 'varchar' })
+  @ApiProperty({ description: '출석 시간', type: 'string' })
+  time: string;
 
-    @ManyToOne(() => Attendee, (attendee) => attendee.schedules)
-    @JoinColumn({ name: 'attendeeId', referencedColumnName: 'id' })
-    @ApiProperty({ type: () => Attendee })
-    attendee: Attendee;
+  @ManyToOne(() => Attendee, (attendee) => attendee.schedules)
+  @JoinColumn({ name: 'attendeeId', referencedColumnName: 'id' })
+  @ApiProperty({ type: () => Attendee })
+  attendee: Attendee;
 }
