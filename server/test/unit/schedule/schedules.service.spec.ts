b2cb0d55f7cbde501583763f4c31573f290554c6
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2024-01-13 17:27:15 +0900

    [ADD] setupTest 구현

--- server/test/unit/schedule/schedules.service.spec.ts
+++ server/test/unit/schedule/schedules.service.spec.ts
@@ -7,6 +7,7 @@ import { User } from '../../../src/users/entities/user.entity';
 import { AttendeesService } from '../../../src/attendees/attendees.service';
 import { Attendance } from '../../../src/attendances/entities/attendance.entity';
 import { Schedule } from '../../../src/schedules/entities/schedule.entity';
+import { AttendanceType } from '../../../src/attendances/const/attendance-type.enum';
 
 describe('SchedulesService', () => {
   let module: TestingModule;
@@ -29,15 +30,6 @@ describe('SchedulesService', () => {
     userRepository = module.get(getRepositoryToken(User));
   });
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      imports: [TestModule, TypeOrmModule.forFeature([Schedule, Attendee])],
-      providers: [SchedulesService],
-    }).compile();
-
-    service = module.get<SchedulesService>(SchedulesService);
-  });
-
   afterAll(async () => {
     await module.close();
   });
@@ -46,7 +38,63 @@ describe('SchedulesService', () => {
     expect(service).toBeDefined();
   });
 
+  describe('Create Schedules Test', () => {
+    it('선택한 요일과 시간으로 출석 대상의 스케쥴을 생성한다.', async () => {});
+  });
+
+  async function setupTest() {
+    await attendanceRepository.query('DELETE FROM attendance;');
+    await userRepository.query(`DELETE FROM user;`);
+
+    const user_1 = new User();
+    user_1.id = 'user id 1';
+    user_1.username = 'test id';
+    user_1.password = 'testPWD';
+    user_1.mobileNumber = '010-8098-1398';
+    user_1.name = 'test name';
+    user_1.createId = 'user id';
+
+    await userRepository.save(user_1);
+
+    const attendance_1 = new Attendance();
+    attendance_1.id = 'testAttendanceId';
+    attendance_1.title = 'testAttendanceTitle';
+    attendance_1.description = 'description';
+    attendance_1.type = AttendanceType.WEEKDAY;
+    attendance_1.createId = 'user id 1';
+    attendance_1.createdAt = new Date();
+
+    const attendance_2 = new Attendance();
+    attendance_2.id = 'notTestAttendanceId';
+    attendance_2.title = 'testAttendanceTitle2';
+    attendance_2.description = 'description';
+    attendance_2.type = AttendanceType.WEEKDAY;
+    attendance_2.createId = 'user id 1';
+    attendance_2.createdAt = new Date();
+
+    await attendanceRepository.save(attendance_1);
+    await attendanceRepository.save(attendance_2);
+
+    const attendee_1 = new Attendee();
+    attendee_1.name = 'Attendee Name 1';
+    attendee_1.attendanceId = attendance_1.id;
+    attendee_1.description = 'Attendee 1 description';
+    attendee_1.age = 10;
+    attendee_1.createId = user_1.id;
+
+    const attendee_2 = new Attendee();
+    attendee_2.name = 'Attendee Name 2';
+    attendee_2.attendanceId = attendance_2.id;
+    attendee_2.description = 'Attendee 2 description';
+    attendee_2.age = 20;
+    attendee_2.createId = user_1.id;
+
+    await attendeeRepository.save(attendee_1);
+    await attendeeRepository.save(attendee_2);
+  }
+
   async function clear() {
+    await scheduleRepository.query('DELETE FROM schedule;');
     await attendeeRepository.query('DELETE FROM attendee;');
     await attendanceRepository.query('DELETE FROM attendance');
     await userRepository.query(`DELETE FROM user;`);
