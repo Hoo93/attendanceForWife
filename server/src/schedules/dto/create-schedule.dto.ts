364e41f55305f19aa9fe24ec2b4f94d890847720
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2024-01-13 17:32:55 +0900

    [ADD] toEntity 구현

--- server/src/schedules/dto/create-schedule.dto.ts
+++ server/src/schedules/dto/create-schedule.dto.ts
@@ -3,6 +3,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { DayType } from '../const/day-type.enum';
 import { Attendee } from '../../attendees/entities/attendee.entity';
 import { IsEnum, IsNotEmpty, IsString, Matches } from 'class-validator';
+import { Schedule } from '../entities/schedule.entity';
 
 export class CreateScheduleDto {
   @IsString()
@@ -15,4 +16,12 @@ export class CreateScheduleDto {
   @IsString()
   @Matches(/^\d{4}$/)
   time: string;
+
+  toEntity() {
+    const schedule = new Schedule();
+    schedule.attendeeId = this.attendeeId;
+    schedule.day = this.day;
+    schedule.time = this.time;
+    return schedule;
+  }
 }
