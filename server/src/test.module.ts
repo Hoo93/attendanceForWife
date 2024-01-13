e8bae4aee3d62dfdb2da152414513bdbb3f46cf6
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2024-01-13 16:17:08 +0900

    [ADD] test 모듈에 Schedule 추가

--- server/src/test.module.ts
+++ server/src/test.module.ts
@@ -11,18 +11,21 @@ import { AttendancesModule } from './attendances/attendances.module';
 import { UserAttendance } from './attendances/entities/user-attendance.entity';
 import { Attendee } from './attendees/entities/attendee.entity';
 import { AttendeesModule } from './attendees/attendees.module';
+import {SchedulesModule} from "./schedules/schedules.module";
+import {Schedule} from "./schedules/entities/schedule.entity";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       ...ormConfigTest,
-      entities: [User, Role, Attendance, UserAttendance, Attendee],
+      entities: [User, Role, Attendance, UserAttendance, Attendee, Schedule],
     }),
     UsersModule,
     AuthModule,
     RolesModule,
     AttendancesModule,
     AttendeesModule,
+    SchedulesModule
   ],
 })
 export class TestModule {}
