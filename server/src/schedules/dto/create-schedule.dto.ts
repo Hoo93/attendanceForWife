fe8b6586a0118fe43aabcb1c5853fead1750e9ca
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2024-01-13 17:55:49 +0900

    [ADD] toEntity 메소드 createId 파라미터 추가

--- server/src/schedules/dto/create-schedule.dto.ts
+++ server/src/schedules/dto/create-schedule.dto.ts
@@ -4,6 +4,7 @@ import { DayType } from '../const/day-type.enum';
 import { Attendee } from '../../attendees/entities/attendee.entity';
 import { IsEnum, IsNotEmpty, IsString, Matches } from 'class-validator';
 import { Schedule } from '../entities/schedule.entity';
+import { share } from 'rxjs';
 
 export class CreateScheduleDto {
   @IsString()
@@ -17,11 +18,12 @@ export class CreateScheduleDto {
   @Matches(/^\d{4}$/)
   time: string;
 
-  toEntity() {
+  toEntity(createId: string) {
     const schedule = new Schedule();
     schedule.attendeeId = this.attendeeId;
     schedule.day = this.day;
     schedule.time = this.time;
+    schedule.createId = createId;
     return schedule;
   }
 }
