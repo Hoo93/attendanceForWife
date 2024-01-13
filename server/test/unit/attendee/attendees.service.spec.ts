471106bccb6716424d380a40cd9c592e505bd1a5
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2024-01-13 17:26:46 +0900

    [FIX] 코드 정리

--- server/test/unit/attendee/attendees.service.spec.ts
+++ server/test/unit/attendee/attendees.service.spec.ts
@@ -1,18 +1,22 @@
-import {Test, TestingModule} from '@nestjs/testing';
-import {AttendeesService} from '../../../src/attendees/attendees.service';
-import {TestModule} from '../../../src/test.module';
-import {getDataSourceToken, getRepositoryToken, TypeOrmModule} from '@nestjs/typeorm';
-import {Attendee} from '../../../src/attendees/entities/attendee.entity';
-import {Attendance} from '../../../src/attendances/entities/attendance.entity';
-import {User} from '../../../src/users/entities/user.entity';
-import {CreateAttendeeDto} from '../../../src/attendees/dto/create-attendee.dto';
-import {AttendanceType} from "../../../src/attendances/const/attendance-type.enum";
-import {DataSource} from "typeorm";
-import * as module from "module";
-import {UpdateAttendeeDto} from "../../../src/attendees/dto/update-attendee.dto";
+import { Test, TestingModule } from '@nestjs/testing';
+import { AttendeesService } from '../../../src/attendees/attendees.service';
+import { TestModule } from '../../../src/test.module';
+import {
+  getDataSourceToken,
+  getRepositoryToken,
+  TypeOrmModule,
+} from '@nestjs/typeorm';
+import { Attendee } from '../../../src/attendees/entities/attendee.entity';
+import { Attendance } from '../../../src/attendances/entities/attendance.entity';
+import { User } from '../../../src/users/entities/user.entity';
+import { CreateAttendeeDto } from '../../../src/attendees/dto/create-attendee.dto';
+import { AttendanceType } from '../../../src/attendances/const/attendance-type.enum';
+import { DataSource } from 'typeorm';
+import * as module from 'module';
+import { UpdateAttendeeDto } from '../../../src/attendees/dto/update-attendee.dto';
 
 describe('AttendeesService', () => {
-  let module:TestingModule
+  let module: TestingModule;
   let service: AttendeesService;
   let attendeeRepository;
   let attendanceRepository;
@@ -28,7 +32,7 @@ describe('AttendeesService', () => {
     attendeeRepository = module.get(getRepositoryToken(Attendee));
     attendanceRepository = module.get(getRepositoryToken(Attendance));
     userRepository = module.get(getRepositoryToken(User));
-  })
+  });
 
   beforeEach(async () => {
     await setupTest();
@@ -40,8 +44,8 @@ describe('AttendeesService', () => {
   });
 
   afterAll(async () => {
-    await module.close()
-  })
+    await module.close();
+  });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
@@ -50,16 +54,18 @@ describe('AttendeesService', () => {
   describe('createAttendee Test ', () => {
     it('출석 대상 테이블에 출석 대상자를 생성한다.', async () => {
       // given
-      const attendeeDto = createAttendeeDto('test name','testAttendanceId','this is first attendee',15)
+      const attendeeDto = createAttendeeDto(
+        'test name',
+        'testAttendanceId',
+        'this is first attendee',
+        15,
+      );
 
       const user = new User();
       user.id = 'user id 1';
 
       // when
-      const createdAttendee = await service.createAttendee(
-          attendeeDto,
-          user,
-      );
+      const createdAttendee = await service.createAttendee(attendeeDto, user);
 
       // then
       expect(createdAttendee?.name).toBe('test name');
@@ -78,9 +84,24 @@ describe('AttendeesService', () => {
       const user_1 = new User();
       user_1.id = 'user id 1';
 
-      const attendeeDto_1 = createAttendeeDto('가나다','testAttendanceId','가나다 학생',3)
-      const attendeeDto_2 = createAttendeeDto('라마바','testAttendanceId','라마바 학생',4)
-      const attendeeDto_3 = createAttendeeDto('아자차','notTestAttendanceId','아자차 학생',5)
+      const attendeeDto_1 = createAttendeeDto(
+        '가나다',
+        'testAttendanceId',
+        '가나다 학생',
+        3,
+      );
+      const attendeeDto_2 = createAttendeeDto(
+        '라마바',
+        'testAttendanceId',
+        '라마바 학생',
+        4,
+      );
+      const attendeeDto_3 = createAttendeeDto(
+        '아자차',
+        'notTestAttendanceId',
+        '아자차 학생',
+        5,
+      );
 
       await service.createAttendee(attendeeDto_1, user_1);
       await service.createAttendee(attendeeDto_2, user_1);
@@ -99,8 +120,8 @@ describe('AttendeesService', () => {
     });
   });
 
-  describe('update TEST', () =>{
-    it('이름, 설명, 나이 중 원하는 값을 수정할 수 있다.',async () => {
+  describe('update TEST', () => {
+    it('이름, 설명, 나이 중 원하는 값을 수정할 수 있다.', async () => {
       // Given
       const attendance = new Attendance();
       attendance.id = 'testAttendanceId';
@@ -108,40 +129,49 @@ describe('AttendeesService', () => {
       const user_1 = new User();
       user_1.id = 'user id 1';
 
-      const attendee = createAttendee('가나다','testAttendanceId','가나다 학생',3,user_1.id)
+      const attendee = createAttendee(
+        '가나다',
+        'testAttendanceId',
+        '가나다 학생',
+        3,
+        user_1.id,
+      );
 
-      const createdAttendee = await attendeeRepository.save(attendee)
+      const createdAttendee = await attendeeRepository.save(attendee);
 
       // When
-      const updateDto = new UpdateAttendeeDto()
-      updateDto.name = '수정된'
-      updateDto.description = '수정되었습니다'
-      updateDto.age = 99
+      const updateDto = new UpdateAttendeeDto();
+      updateDto.name = '수정된';
+      updateDto.description = '수정되었습니다';
+      updateDto.age = 99;
 
-      const updatedAttendee = await service.update(createdAttendee.id,updateDto);
+      const updatedAttendee = await service.update(
+        createdAttendee.id,
+        updateDto,
+      );
 
       // Then
-      expect(updatedAttendee.name).toBe('수정된')
-      expect(updatedAttendee.description).toBe('수정되었습니다')
-      expect(updatedAttendee.age).toBe(99)
-    })
-  })
+      expect(updatedAttendee.name).toBe('수정된');
+      expect(updatedAttendee.description).toBe('수정되었습니다');
+      expect(updatedAttendee.age).toBe(99);
+    });
+  });
   async function setupTest() {
     await attendanceRepository.query('DELETE FROM attendance;');
     await userRepository.query(`DELETE FROM user;`);
 
-    const user_1 = new User()
+    const user_1 = new User();
     user_1.id = 'user id 1';
     user_1.username = 'test id';
     user_1.password = 'testPWD';
     user_1.mobileNumber = '010-8098-1398';
     user_1.name = 'test name';
-    user_1.createId ='user id';
+    user_1.createId = 'user id';
 
-    await userRepository.save(user_1)
+    await userRepository.save(user_1);
 
-    await attendanceRepository
-    const attendance_1 = new Attendance()
+    await attendanceRepository;
+    const attendance_1 = new Attendance();
     attendance_1.id = 'testAttendanceId';
     attendance_1.title = 'testAttendanceTitle';
     attendance_1.description = 'description';
@@ -149,7 +179,7 @@ describe('AttendeesService', () => {
     attendance_1.createId = 'user id 1';
     attendance_1.createdAt = new Date();
 
-    const attendance_2 = new Attendance()
+    const attendance_2 = new Attendance();
     attendance_2.id = 'notTestAttendanceId';
     attendance_2.title = 'testAttendanceTitle2';
     attendance_2.description = 'description';
@@ -157,9 +187,8 @@ describe('AttendeesService', () => {
     attendance_2.createId = 'user id 1';
     attendance_2.createdAt = new Date();
 
-    await attendanceRepository.save(attendance_1)
-    await attendanceRepository.save(attendance_2)
-
+    await attendanceRepository.save(attendance_1);
+    await attendanceRepository.save(attendance_2);
   }
 
   async function clear() {
@@ -168,21 +197,21 @@ describe('AttendeesService', () => {
     await userRepository.query(`DELETE FROM user;`);
   }
 });
-function createAttendeeDto(name,attendanceId,description,age) {
+function createAttendeeDto(name, attendanceId, description, age) {
   const createAttendeeDto = new CreateAttendeeDto();
-  createAttendeeDto.name = name ;
+  createAttendeeDto.name = name;
   createAttendeeDto.attendanceId = attendanceId;
   createAttendeeDto.description = description;
   createAttendeeDto.age = age;
-  return createAttendeeDto
+  return createAttendeeDto;
 }
 
-function createAttendee(name,attendanceId,description,age,createId) {
-  const attendee = new Attendee()
-  attendee.name = name ;
+function createAttendee(name, attendanceId, description, age, createId) {
+  const attendee = new Attendee();
+  attendee.name = name;
   attendee.attendanceId = attendanceId;
   attendee.description = description;
   attendee.age = age;
   attendee.createId = createId;
-  return attendee
+  return attendee;
 }
