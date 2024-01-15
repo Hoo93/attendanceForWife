4760c77557e4dff80585fe2734db57af036b4a0b
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2024-01-15 13:28:23 +0900

    [ADD] toEntity method 구현

--- server/src/records/dto/create-record.dto.ts
+++ server/src/records/dto/create-record.dto.ts
@@ -3,6 +3,7 @@ import { AttendanceStatus } from '../record-type.enum';
 import { ApiProperty } from '@nestjs/swagger';
 import { DayType } from '../../schedules/const/day-type.enum';
 import { IsDate, IsEnum, IsOptional, IsString, Matches } from 'class-validator';
+import { Record } from '../entities/record.entity';
 
 export class CreateRecordDto {
   @IsEnum(AttendanceStatus)
@@ -20,4 +21,15 @@ export class CreateRecordDto {
   @IsOptional()
   @IsString()
   lateReason: string;
+
+  toEntity(createId: string) {
+    const record = new Record();
+    record.status = this.status;
+    record.datetime = this.datetime;
+    record.day = this.day;
+    record.attendeeId = this.attendeeId;
+    record.createId = createId;
+    record.lateReason = this?.lateReason;
+    return record;
+  }
 }
