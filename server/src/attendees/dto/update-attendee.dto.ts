a6486343944538cd0d3697d198beb8544715da57
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2024-01-15 23:56:45 +0900

    [ADD] updateId, updatedAt 파라미터 추가

--- server/src/attendees/dto/update-attendee.dto.ts
+++ server/src/attendees/dto/update-attendee.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateAttendeeDto } from './create-attendee.dto';
 
-export class UpdateAttendeeDto extends PartialType(CreateAttendeeDto) {}
+export class UpdateAttendeeDto extends PartialType(CreateAttendeeDto) {
+  updateId: string;
+
+  updatedAt: Date;
+}
