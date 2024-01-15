2f6acaef4ae1c4a9c33b1699c34a2044475d05e4
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2024-01-16 00:28:18 +0900

    [ADD] createRecord / RoleGuard, swagger 적용

--- server/src/records/records.controller.ts
+++ server/src/records/records.controller.ts
@@ -6,19 +6,40 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { RecordsService } from './records.service';
 import { CreateRecordDto } from './dto/create-record.dto';
 import { UpdateRecordDto } from './dto/update-record.dto';
 import { User } from '../users/entities/user.entity';
 import { GetUser } from '../common/user.decorator';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { Attendee } from '../attendees/entities/attendee.entity';
+import { Record } from './entities/record.entity';
+import { RoleGuard } from '../roles/role.guard';
+import { Roles } from '../roles/role.decorator';
+import { RoleType } from '../roles/entities/role-type.enum';
 
 @Controller('records')
 export class RecordsController {
   constructor(private readonly recordsService: RecordsService) {}
 
   @Post()
-  create(@Body() createRecordDto: CreateRecordDto, @GetUser() user: User) {
+  @ApiOperation({
+    description: '출석기록 생성',
+    summary: '출석기록 생성 요약',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '출석기록 생성',
+    type: Record,
+  })
+  @UseGuards(RoleGuard)
+  @Roles(RoleType.MASTER, RoleType.GENERAL, RoleType.MANAGER)
+  async createRecord(
+    @Body() createRecordDto: CreateRecordDto,
+    @GetUser() user: User,
+  ) {
     return this.recordsService.create(createRecordDto, user);
   }
 
