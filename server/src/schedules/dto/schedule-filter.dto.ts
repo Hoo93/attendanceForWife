740b76447f2ce822ec1cc56dd0163146f488bb88
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2024-01-26 08:51:35 +0900

    [ADD] schedule-filter.dto / schedule 검색 파라미터 dto 구현

---
+++ server/src/schedules/dto/schedule-filter.dto.ts
@@ -0,0 +1,41 @@
+import { Column, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { DayType } from '../const/day-type.enum';
+import { Attendee } from '../../attendees/entities/attendee.entity';
+import { IsEnum, IsNotEmpty, IsString, Matches } from 'class-validator';
+import { Schedule } from '../entities/schedule.entity';
+import { share } from 'rxjs';
+import { IsTimeFormat } from '../../common/isTimeformat.decorator';
+import { Optional } from '@nestjs/common';
+
+export class ScheduleFilterDto {
+  @IsEnum(DayType, { each: true })
+  @Optional()
+  @ApiProperty({
+    description: '출석부 요일 배열',
+    type: 'Array',
+    nullable: true,
+    example: [DayType.MONDAY, DayType.TUESDAY],
+  })
+  days: DayType[];
+
+  @IsTimeFormat()
+  @Optional()
+  @ApiProperty({
+    description: '검색 시작기준 시간 (format:hhmm)',
+    type: 'string',
+    nullable: true,
+    example: '1200',
+  })
+  timeFrom: string;
+
+  @IsTimeFormat()
+  @Optional()
+  @ApiProperty({
+    description: '검색 종료기준 시간 (format:hhmm)',
+    type: 'string',
+    nullable: true,
+    example: '1815',
+  })
+  timeTo: string;
+}
