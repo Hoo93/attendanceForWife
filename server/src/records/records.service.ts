057e35116efc08d441bead9dbcd77b3995a456c7
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2024-02-02 23:48:15 +0900

    [ADD] 지각이 아닌 경우 lateReason 삭제 구현

--- server/src/records/records.service.ts
+++ server/src/records/records.service.ts
@@ -4,9 +4,10 @@ import { UpdateRecordDto } from './dto/update-record.dto';
 import { User } from '../users/entities/user.entity';
 import { Record } from './entities/record.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { In, Repository } from 'typeorm';
+import { In, InsertResult, Repository } from 'typeorm';
 import { DeleteRecordDto } from './dto/delete-record.dto';
 import { CreateAllRecordDto } from './dto/createAll-record.dto';
+import { AttendanceStatus } from './record-type.enum';
 
 @Injectable()
 export class RecordsService {
@@ -17,12 +18,16 @@ export class RecordsService {
   async create(createRecordDto: CreateRecordDto, user: User) {
     const record = createRecordDto.toEntity(user.id);
 
-    const result = await this.recordRepository.upsert(record, {
+    if (record.status !== AttendanceStatus.ABSENT) {
+      delete record.lateReason;
+    }
+
+    const result: InsertResult = await this.recordRepository.upsert(record, {
       conflictPaths: ['attendeeId', 'date'],
       upsertType: 'on-conflict-do-update',
     });
 
-    return this.findOneById(result.raw.id);
+    return this.findOneById(result.raw?.insertId);
   }
 
   async createAll(createAllrecordDto: CreateAllRecordDto, user: User): Promise<number> {
@@ -45,19 +50,6 @@ export class RecordsService {
       ],
     );
     return result.affectedRows;
-  }
-
-  private getDate(date: Date) {
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDay();
-
-    // 한 자리수 월과 일에 선행하는 0 추가
-    const formattedMonth = month < 10 ? `0${month}` : month;
-    const formattedDay = day < 10 ? `0${day}` : day;
-
-    // YYYY-MM-DD 형식으로 변환
-    return `${year}-${formattedMonth}-${formattedDay}`;
   }
 
   findOneById(id: number) {
