39e16b33d4bf7c571752cd158307d97e373bfe4a
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2024-02-04 23:42:29 +0900

    [EDIT] pagination swagger 문서 작성 및 검증 로직 수정

--- server/src/common/pagination.ts
+++ server/src/common/pagination.ts
@@ -1,4 +1,22 @@
-export interface Pagination {
-  skip: number;
-  take: number;
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsPositive, Max, Min } from 'class-validator';
+
+export class Pagination {
+  @ApiProperty({
+    type: 'number',
+    description: '스킵할 개수',
+    default: 0,
+  })
+  @IsNumber()
+  @Min(0)
+  skip: number = 0;
+
+  @ApiProperty({
+    type: 'number',
+    description: '가지고 올 개수',
+    default: 10,
+  })
+  @IsPositive()
+  @Max(100)
+  take: number = 10;
 }
