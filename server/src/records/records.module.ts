9ebbcd07fb83ee249e382ac483478f7370d97b44
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2024-02-08 00:14:16 +0900

    [ADD] ExcelService 주입

--- server/src/records/records.module.ts
+++ server/src/records/records.module.ts
@@ -4,10 +4,11 @@ import { RecordsController } from './records.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Schedule } from '../schedules/entities/schedule.entity';
 import { Record } from './entities/record.entity';
+import { ExcelService } from '../common/excel.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Record])],
   controllers: [RecordsController],
-  providers: [RecordsService],
+  providers: [RecordsService, ExcelService],
 })
 export class RecordsModule {}
