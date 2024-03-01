243e5d2570c49539d7751e4b7e29ae563cbb43f5
Hoo93 <sksk8922@gmail.com>
2024-03-02 01:09:52 +0900

    [ADD] create-invitation.dto 구현

--- server/src/invitations/dto/create-invitation.dto.ts
+++ server/src/invitations/dto/create-invitation.dto.ts
@@ -1,1 +1,15 @@
-export class CreateInvitationDto {}
+import { IsEnum } from 'class-validator';
+import { AttendanceStatus } from '../../records/record-type.enum';
+import { ApiProperty } from '@nestjs/swagger';
+import { RoleType } from '../../roles/entities/role-type.enum';
+
+export class CreateInvitationDto {
+  @IsEnum(RoleType)
+  @ApiProperty({
+    description: '출석 기록 상태',
+    type: 'enum',
+    enum: RoleType,
+    example: RoleType.READER,
+  })
+  status: RoleType;
+}
