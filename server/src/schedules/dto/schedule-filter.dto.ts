986d28550e82dc444176738450e703b3c2cf3665
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2024-03-02 00:25:25 +0900

    [DOC] swagger 문서 optionalProperty로 수정

--- server/src/schedules/dto/schedule-filter.dto.ts
+++ server/src/schedules/dto/schedule-filter.dto.ts
@@ -1,8 +1,8 @@
 import { Column, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { DayType } from '../const/day-type.enum';
 import { Attendee } from '../../attendees/entities/attendee.entity';
-import { IsEnum, IsNotEmpty, IsString, Matches } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsOptional, IsString, Matches } from 'class-validator';
 import { Schedule } from '../entities/schedule.entity';
 import { share } from 'rxjs';
 import { IsTimeFormat } from '../../common/isTimeformat.decorator';
@@ -10,32 +10,32 @@ import { Optional } from '@nestjs/common';
 
 export class ScheduleFilterDto {
   @IsEnum(DayType, { each: true })
-  @Optional()
-  @ApiProperty({
+  @IsOptional()
+  @ApiPropertyOptional({
     description: '출석부 요일 배열',
     type: 'Array',
     nullable: true,
     example: [DayType.MONDAY, DayType.TUESDAY],
   })
-  days: DayType[];
+  days?: DayType[];
 
   @IsTimeFormat()
-  @Optional()
-  @ApiProperty({
+  @IsOptional()
+  @ApiPropertyOptional({
     description: '검색 시작기준 시간 (format:hhmm)',
     type: 'string',
     nullable: true,
     example: '1200',
   })
-  timeFrom: string;
+  timeFrom?: string;
 
   @IsTimeFormat()
-  @Optional()
-  @ApiProperty({
+  @IsOptional()
+  @ApiPropertyOptional({
     description: '검색 종료기준 시간 (format:hhmm)',
     type: 'string',
     nullable: true,
     example: '1815',
   })
-  timeTo: string;
+  timeTo?: string;
 }
