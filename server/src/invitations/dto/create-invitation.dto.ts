3e3b7bc01d846ca3fd1d448e72bd759db12502da
Hoo93 <sksk8922@gmail.com>
2024-03-02 01:16:20 +0900

    [EDIT] attendanceId 속성 추가

--- server/src/invitations/dto/create-invitation.dto.ts
+++ server/src/invitations/dto/create-invitation.dto.ts
@@ -1,4 +1,4 @@
-import { IsEnum } from 'class-validator';
+import { IsEnum, IsString } from 'class-validator';
 import { AttendanceStatus } from '../../records/record-type.enum';
 import { ApiProperty } from '@nestjs/swagger';
 import { RoleType } from '../../roles/entities/role-type.enum';
@@ -6,10 +6,18 @@ import { RoleType } from '../../roles/entities/role-type.enum';
 export class CreateInvitationDto {
   @IsEnum(RoleType)
   @ApiProperty({
-    description: '출석 기록 상태',
+    description: '선택 권한으로 출석부에 초대',
     type: 'enum',
     enum: RoleType,
     example: RoleType.READER,
   })
   status: RoleType;
+
+  @IsString()
+  @ApiProperty({
+    description: '초대할 출석부의 ID',
+    type: 'string',
+    example: 'attendance-uuid',
+  })
+  attendanceId: string;
 }
