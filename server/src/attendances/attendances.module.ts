092b903da6203f6021e49bbbc0d6b3a117558d65
Hoo93 <sksk8922@gmail.com>
2024-03-05 23:54:34 +0900

    [ADD] 신규 출석부 생성 시 access_token 재발급 로직 테스트코드 추가 및 테스트 환경 설정

--- server/src/attendances/attendances.module.ts
+++ server/src/attendances/attendances.module.ts
@@ -4,10 +4,22 @@ import { AttendancesController } from './attendances.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Attendance } from './entities/attendance.entity';
 import { UserAttendance } from './entities/user-attendance.entity';
+import { AuthService } from '../auth/auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from '../auth/const/auth.const';
+import { User } from '../users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Attendance, UserAttendance])],
+  imports: [
+    TypeOrmModule.forFeature([Attendance, UserAttendance, User]),
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: jwtConstants.expiresIn },
+    }),
+  ],
   controllers: [AttendancesController],
-  providers: [AttendancesService],
+  providers: [AttendancesService, AuthService],
 })
 export class AttendancesModule {}
