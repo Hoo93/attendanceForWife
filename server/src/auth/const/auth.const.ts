c044476e9580145d4ff2e4e8e6587c602fb6f3ef
박상후 <sksk8922@gmail.com>
2024-03-22 08:45:08 +0000

    EDIT: 토큰 만료시간 변경

--- server/src/auth/const/auth.const.ts
+++ server/src/auth/const/auth.const.ts
@@ -2,6 +2,6 @@ export const SALT = 13;
 export const jwtConstants = {
   accessTokenSecret: 'This is my super secret',
   refreshTokenSecret: 'This is my super super secret',
-  accessTokenExpiresIn: '300s',
+  accessTokenExpiresIn: '60s',
   refreshTokenExpiresIn: '1d',
 };
