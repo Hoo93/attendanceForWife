2083c635c1851382bd0308b07efed7eca86d92de
Hoo93 <sksk8922@gmail.com>
2024-03-24 00:28:45 +0900

    [EDIT] accessToken 만료기간 5분으로 변경

--- server/src/auth/const/auth.const.ts
+++ server/src/auth/const/auth.const.ts
@@ -2,7 +2,7 @@ export const SALT = 13;
 export const jwtConstants = {
   accessTokenSecret: 'This is my super secret',
   refreshTokenSecret: 'This is my super super secret',
-  accessTokenExpiresIn: '60s',
+  accessTokenExpiresIn: '300s',
   refreshTokenExpiresIn: '1d',
   autoLoginRefreshTokenExpiresIn: '30d',
 };
