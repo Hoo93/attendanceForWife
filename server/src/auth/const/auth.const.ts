56df3ca0a0219087398122a8bf23a22a7ef72e97
Hoo93 <sksk8922@gmail.com>
2024-03-29 23:57:05 +0900

    [EDIT] 엑세스 토큰 만료시간 수정

--- server/src/auth/const/auth.const.ts
+++ server/src/auth/const/auth.const.ts
@@ -2,7 +2,7 @@ export const SALT = 13;
 export const jwtConstants = {
   accessTokenSecret: 'This is my super secret',
   refreshTokenSecret: 'This is my super super secret',
-  accessTokenExpiresIn: '10s',
+  accessTokenExpiresIn: '300s',
   refreshTokenExpiresIn: '1d',
   autoLoginRefreshTokenExpiresIn: '30d',
 };
