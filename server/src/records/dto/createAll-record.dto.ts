f12c77059bd9a36fa8a0eb90f5aa6d5f26eabaa7
Hoo93 <sksk8922@gmail.com>
2024-03-31 00:35:07 +0900

    [FIX] 선택한 요일 출석 기록 전체 생성 DTO 재구현

---
+++ server/src/records/dto/createAll-record.dto.ts
@@ -0,0 +1,53 @@
+import { Column } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { DayType } from '../../schedules/const/day-type.enum';
+import { IsDate, IsEnum, IsNotEmpty, IsOptional, IsString, Matches } from 'class-validator';
+import { Record } from '../entities/record.entity';
+import { Transform } from 'class-transformer';
+import { AttendanceStatus } from '../const/record-type.enum';
+
+export class CreateAllRecordDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: '출석부 PK',
+    type: 'string',
+    example: 'uuid-1234-uuid',
+  })
+  attendanceId: string;
+
+  @IsEnum(AttendanceStatus)
+  @ApiProperty({
+    description: '출석 기록 상태',
+    type: 'enum',
+    enum: AttendanceStatus,
+    example: AttendanceStatus.PRESENT,
+  })
+  status: AttendanceStatus;
+
+  @IsDate()
+  @ApiProperty({
+    description: '출석날짜',
+    type: 'date',
+    example: '2024-12-03',
+  })
+  date: string;
+
+  @IsEnum(DayType)
+  @ApiProperty({
+    description: '출석요일',
+    type: 'enum',
+    enum: DayType,
+    example: DayType.MONDAY,
+  })
+  day: DayType;
+
+  toEntity(createId: string) {
+    const record = new Record();
+    record.status = this.status;
+    record.date = this.date;
+    record.day = this.day;
+    record.createId = createId;
+    return record;
+  }
+}
