85aa55efe83cab29319f000b07adbe66f728f71e
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-04-02 11:32:02 +0900

    ADD: 출석부 목록 페이지 개발 (작업 중)

--- client/src/app/attendances/page.tsx
+++ client/src/app/attendances/page.tsx
@@ -1,23 +1,18 @@
 'use client';
 
 import 'dayjs/locale/ko'; // 한국어 locale 설정
-import dayjs from 'dayjs';
-import AttendanceApiClient from '@/api/AttendanceApiClient';
-import { Box, Container, Typography, styled, useTheme } from '@mui/material';
+
+import { Box, Container, Typography, styled } from '@mui/material';
 import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+
+import AttendanceApiClient from '@/api/AttendanceApiClient';
+import Image from 'next/image';
 import React from 'react';
-import { useState } from 'react';
-import { useMediaQuery } from 'react-responsive';
+import dayjs from 'dayjs';
 
 dayjs.locale('ko');
 
 const Index = () => {
-    const isSmall = useMediaQuery({
-        query: '(min-width: 393px)',
-    });
-
-    const [isCreate, setIsCreate] = useState<boolean>(false);
-    const theme = useTheme();
     // 오늘 날짜
     const today = dayjs();
     const todayFormat = today.format('YYYY년 MM월 DD일 dddd');
@@ -31,24 +26,146 @@ const Index = () => {
         },
     });
 
-    //   const {} = useInfiniteQuery({
-    //     queryKey: ["attendancy-list"],
-    //     queryFn: async ({ pageParam }) =>
-    //       await AttendancyApiClient.getInstance().getAttendanceList(),
-    //   });
-
     return (
         <ContainerST>
             <StyledBoxST>
-                <Typography>{todayFormat}</Typography>
-                <Typography>김범수님, 안녕하세요.</Typography>
-                <Box display={'flex'} flexDirection={'column'} gap={'10px'}>
-                    <Box display={'flex'} justifyContent={'space-between'}>
-                        <Box></Box>
-                        <Box></Box>
-                    </Box>
+                <Box>
+                    <Typography
+                        sx={{
+                            fontFamily: 'Noto Sans',
+                            fontSize: '14px',
+                            lineHeight: '19.07px',
+                            color: '#797979',
+                        }}
+                    >
+                        {todayFormat}
+                    </Typography>
+                    <Typography
+                        sx={{
+                            fontFamily: 'Noto Sans',
+                            fontSize: '20px',
+                            fontWeight: 600,
+                            lineHeight: '27.24px',
+                        }}
+                    >
+                        김범수님, 안녕하세요.
+                    </Typography>
+                </Box>
+                <Box
+                    display={'flex'}
+                    sx={{
+                        width: '320px',
+                        height: '200',
+                        margin: 'auto',
+                        gap: '10px',
+                        justifyContent: 'space-between',
+                        flexWrap: 'wrap',
+                        padding: '0px 4px',
+                    }}
+                >
+                    {attendancyList?.items.map((item: any) => {
+                        return (
+                            <Box
+                                sx={{
+                                    width: '150px',
+                                    height: '185px',
+                                    border: '1px solid green',
+                                    borderRadius: '4px',
+                                    display: 'flex',
+                                    gap: '12px',
+                                    cursor: 'pointer',
+                                    flexDirection: 'column',
+                                    alignItems: 'center',
+                                    padding: '4px',
+                                }}
+                            >
+                                <Image
+                                    src={'/images/sckeleton-image.svg'}
+                                    width={142}
+                                    height={102}
+                                    alt="스켈레톤 이미지"
+                                    onClick={() =>
+                                        alert('준비중인 기능입니다.')
+                                    }
+                                />
+                                <Box
+                                    width={'100%'}
+                                    sx={{
+                                        padding: '0px 7px',
+                                        overflow: 'hidden',
+                                        whiteSpace: 'nowrap',
+                                        textOverflow: 'ellipsis',
+                                    }}
+                                >
+                                    <Typography
+                                        sx={{
+                                            fontFamily: 'Noto Sans',
+                                            fontSize: '16px',
+                                            fontWeight: 600,
+                                            lineHeight: '21.79px',
+                                            overflow: 'hidden',
+                                            whiteSpace: 'nowrap',
+                                            textOverflow: 'ellipsis',
+                                        }}
+                                    >
+                                        {item.attendance.title}
+                                    </Typography>
+                                    <Typography
+                                        sx={{
+                                            fontFamily: 'Noto Sans',
+                                            fontSize: '14px',
+                                            fontWeight: 500,
+                                            color: '#797979',
+                                            lineHeight: '19.07px',
+                                            overflow: 'hidden',
+                                            whiteSpace: 'nowrap',
+                                            textOverflow: 'ellipsis',
+                                        }}
+                                    >
+                                        {item.attendance.description}
+                                    </Typography>
+                                    <Box display={'flex'}>
+                                        <Typography
+                                            sx={{
+                                                fontFamily: 'Noto Sans',
+                                                fontSize: '12px',
+                                                fontWeight: 500,
+                                                color: '#797979',
+                                                lineHeight: '16.34px',
+                                            }}
+                                        >
+                                            월,화,목,금,일
+                                        </Typography>
+                                        <Typography
+                                            sx={{
+                                                fontFamily: 'Noto Sans',
+                                                fontSize: '12px',
+                                                fontWeight: 500,
+                                                alignItems: 'center',
+                                                justifyContent: 'space-between',
+                                                border: '1px solid #F0FFF4',
+                                                lineHeight: '16.34px',
+                                            }}
+                                        >
+                                            {item.attendance.attendeeCount}
+                                        </Typography>
+                                    </Box>
+                                </Box>
+                            </Box>
+                        );
+                    })}
                 </Box>
             </StyledBoxST>
+            <Image
+                src={'/images/ADD-icon.svg'}
+                alt=""
+                width={48}
+                height={48}
+                style={{
+                    marginLeft: 'auto',
+                    cursor: 'pointer',
+                }}
+            />
             {/* {attendancyPage?.data?.items.map((page, index) => {
 
           return;
@@ -67,14 +184,18 @@ const Index = () => {
 export default Index;
 
 const ContainerST = styled(Container)`
+    flex-direction: column;
     display: flex;
     justify-content: space-around;
     align-items: center;
     height: 100%;
+    gap: 10px;
+    margin: auto;
 `;
 const StyledBoxST = styled(Box)`
     display: flex;
-    flex-direction: column;
-    align-items: center;
+    flex-wrap: wrap; /* 요소들을 여러 줄에 걸쳐 정렬 */
     gap: 28px;
+    flex-direction: column;
+    justify-content: space-between; /* 요소들을 간격을 두고 정렬 */
 `;
