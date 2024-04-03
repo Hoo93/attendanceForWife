2bd7ddbb9a9fba361bf6bed9d297e24dc10833d4
harinnn <harinnnkim@gmail.com>
2024-04-03 22:43:45 +0900

    FEAT: not found 페이지 추가

---
+++ client/src/app/not-found.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import styled from '@emotion/styled';
+import Image from 'next/image';
+import { Colors, Images } from '@/styles/globalStyles';
+import Link from 'next/link';
+
+const NotFoundContainer = styled.section`
+    width: 393px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    padding-top: 80px;
+
+    & > img {
+        width: 90%;
+        height: 100px;
+        margin-bottom: 32px;
+    }
+
+    & > h2 {
+        text-align: center;
+        margin-bottom: 48px;
+    }
+
+    & > div {
+        font-size: 18px;
+
+        & > a {
+            font-weight: 600;
+            color: darkgreen;
+            text-underline-position: under;
+        }
+    }
+`;
+
+const NotFound = () => {
+    return (
+        <NotFoundContainer>
+            <Image src={Images.Logo} alt={'logo'} />
+            <h2>
+                죄송합니다.
+                <br />
+                해당 페이지를 찾을 수 없습니다.
+            </h2>
+
+            <div>
+                출석부로 돌아가시려면 <Link href={'/attendances'}>여기</Link>를
+                눌러주세요!
+            </div>
+        </NotFoundContainer>
+    );
+};
+
+export default NotFound;
